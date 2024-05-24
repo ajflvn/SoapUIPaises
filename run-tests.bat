e47881a264735a464ae06e5a8cf05a6ddca3500b
ajflvn <albertojesus.florido@viewnext.com>
2024-05-24 15:48:31 +0200

    Update run-tests.bat

--- run-tests.bat
+++ run-tests.bat
@@ -1,2 +1,4 @@
 @echo off
-echo "Running SoapUI tests"
+echo:Pruebas con SoapUI sobre el servicio web de Informacion de Paises
+echo:cuyo WSDL se encuentra en
+echo:http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL
