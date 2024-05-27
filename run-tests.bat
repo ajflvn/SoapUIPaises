d8aca158f18f09e0032a5d90c2e1bc3720dc1310
ajflvn <albertojesus.florido@viewnext.com>
2024-05-27 09:46:03 +0200

    Delete run-tests.bat

--- run-tests.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo:Pruebas con SoapUI sobre el servicio web de Informacion de Paises
-echo:cuyo WSDL se encuentra en
-echo:http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL
